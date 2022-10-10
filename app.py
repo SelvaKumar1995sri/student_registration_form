653dac3993b021fb102c8bb440d57c21ecda7b50
selva kumar <selvakumar1995sri@gmail.com>
2022-10-10 18:03:01 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,43 +1,28 @@
 from atexit import register
+from re import fullmatch
+from webbrowser import get
 from flask import Flask, render_template, request, url_for, redirect, session
 import pymongo
 
 app = Flask(__name__)
-app.secret_key = "testing"
 client = pymongo.MongoClient("mongodb+srv://gayathri:Sairambaba@cluster1.davwpcs.mongodb.net/?retryWrites=true&w=majority")
-db = client['mydatabase']
-register_collection = db['registration']
-
-@app.route('/')
-def header():
-    return render_template('home.html')
+db = client.get_database('mydatabase')
+records = db.registry
 
 @app.route("/", methods=['post', 'get'])
 def index():
+    
     if request.method == "POST":
-        Name = request.form.get("Name")
-        dob = request.form.get("Date of Birth")
-        gender = request.form.get("Gender")
-        country = request.form.get("Country")
-        degree = request.form.get("Degree")
-        user_found = register_collection.find_one({"Name": Name})
-        if user_found:
-            message = 'There already is a user by that name'
-            return render_template('home.html', message=message)
-        else:
-            user_input = {
-                'Name': Name, 
-                'Date of Birth': dob, 
-                'Gender': gender, 
-                'Country':country, 
-                'Degree':degree
-                }
-            register_collection.insert_one(user_input)
-            record_data = register_collection.find_one({"Name": Name})
-            new_reg = record_data['name']
-            return render_template('home.html',data=new_reg)
+        name = request.form.get("fullname")
+        dob = request.form.get("dob")
+        gender = request.form.get("gender")
+        country = request.form.get("city")
+        degree = request.form.get("degree")
+        data = {'fullname':name, 'dob':dob, 'gender':gender, 'Country':country, 'degree':degree}
+        records.insert_one(data)
+        return render_template('result.html')
 
-    return render_template('home.html',data=new_reg)
+    return render_template('index.html')
 
 if __name__ == "__main__":  
   app.run(debug=True)
